68e4438d4e3ca3f855e1879f7f6f5f45b07af64a
Pedro Jesus <pjesus@wkm.com.br>
2016-09-16 21:20:31 -0300

    Implementação de componente de exibição de status de issue

---
+++ app/assets/javascripts/components/issues/issue_status.jsx
@@ -0,0 +1,24 @@
+const status_props_mapping = {
+  to_do: {
+    color: 'red lighten-2',
+    name: 'To do'
+  },
+  in_progress: {
+    color: 'yellow darken-2',
+    name: 'In progress'
+  },
+  done: {
+    color: 'green lighten-1',
+    name: 'Done'
+  }
+};
+
+const IssueStatus = (props) => {
+  const status_props = status_props_mapping[props.value];
+
+  return (
+    <span className={`issue-status ${status_props.color}`}>
+      {status_props.name}
+    </span>
+  );
+};
